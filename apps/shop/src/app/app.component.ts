2c2152613ba6c3f25480898e48f7cdaf681ea21c
Nick Gernert <nick.gernert@ursaspace.com>
2022-10-24 13:32:15 -0400

    initial

---
+++ apps/shop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pigs-and-flies-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'shop';
+}
